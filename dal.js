a28a8fcf17b79c2bcf8ec5635e3aa84a9f4011ac
James623915 <87282166+James623915@users.noreply.github.com>
2022-04-30 12:49:35 +0700

    Update dal.js

--- dal.js
+++ dal.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const url         = 'mongodb://localhost:27017';
+const url         = 'mongodb+srv://nitidev:t0PgXpFkncuahllM@bank-mern.rqxre.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 let db            = null;
  
 // connect to mongo
